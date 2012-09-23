e9f59e337a00688f5b1d729b0c18b51209b23dc5
Sebastien LANGE <sebastien.lange@syleam.fr>
2012-09-23 13:18:28 +0200

    [FIX] Compare subtotal for blocking not price unit

--- sale.py
+++ sale.py
@@ -27,6 +27,8 @@ from osv import osv
 from osv import fields
 from tools.translate import _
 import logging
+import decimal_precision as dp
+from tools import float_compare
 
 _logger = logging.getLogger('sale_minimal_price')
 
@@ -41,14 +43,13 @@ class sale_order(osv.osv):
         """
         if context is None:
             context = self.pool.get('res.users').context_get(cr, uid)
-
         user = self.pool.get('res.users').browse(cr, uid, uid)
         have_group = user.company_id.unblock_group_id.id in [z.id for z in user.groups_id]
+        precision_digits = self.pool.get('decimal.precision').precision_get(cr, uid, 'Sale Price')
         for so in self.browse(cr, uid, ids):
-            if not have_group and [x.id for x in so.order_line if x.price_unit < x.block_price]:
+            if not have_group and [x.id for x in so.order_line if float_compare(x.price_subtotal, x.block_price_subtotal, precision_digits=precision_digits) < 0]:
                 raise osv.except_osv(_('Validation Error'),
                                      _('You cannot validate the sale order, some lines have a unit price lower than minimal price'))
-
         return super(sale_order, self).action_wait(cr, uid, ids, context)
 
 sale_order()
@@ -57,12 +58,25 @@ sale_order()
 class sale_order_line(osv.osv):
     _inherit = 'sale.order.line'
 
+    def _block_amount_line(self, cr, uid, ids, field_name, arg, context=None):
+        tax_obj = self.pool.get('account.tax')
+        cur_obj = self.pool.get('res.currency')
+        res = {}
+        if context is None:
+            context = {}
+        for line in self.browse(cr, uid, ids, context=context):
+            taxes = tax_obj.compute_all(cr, uid, line.tax_id, line.block_price, line.product_uom_qty, line.order_id.partner_invoice_id.id, line.product_id, line.order_id.partner_id)
+            cur = line.order_id.pricelist_id.currency_id
+            res[line.id] = cur_obj.round(cr, uid, cur, taxes['total'])
+        return res
+
     _columns = {
-        'block_price': fields.float('Block price', help='The product cannot be sale below this price'),
+        'block_price': fields.float('Block price', digits_compute= dp.get_precision('Sale Price'), help='The product cannot be sale below this price'),
+        'block_price_subtotal': fields.function(_block_amount_line, string='Block Subtotal', digits_compute= dp.get_precision('Sale Price')),
     }
 
     _defaults = {
-        'block_price': lambda *a: 0.0,
+        'block_price': 0.0,
     }
 
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
@@ -75,17 +89,12 @@ class sale_order_line(osv.osv):
             cr, uid, ids, pricelist, product, qty,
             uom, qty_uos, uos, name, partner_id,
             lang, update_tax, date_order, packaging, fiscal_position, flag, context=context)
-
         if product:
             cny = self.pool.get('res.users').browse(cr, uid, uid).company_id
             if cny.minimum_pricelist_id:
-                extra = {
-                    'uom': uom,
-                    'date': date_order,
-                }
+                extra = {'uom': uom, 'date': date_order,}
                 price = self.pool.get('product.pricelist').price_get(cr, uid, [cny.minimum_pricelist_id.id],
                                                                      product, qty or 1.0, partner_id, extra)[cny.minimum_pricelist_id.id]
-
                 warning = res['warning']
                 if price is False:
                     warning = {
@@ -93,7 +102,6 @@ class sale_order_line(osv.osv):
                         'message':
                         _("Couldn't find a pricelist line matching this product and quantity.\nYou have to change either the product, the quantity or the pricelist.")
                     }
-
                 res['value']['block_price'] = price or 0.0
                 _logger.debug('Price unit: %f, Block price: %f' % (res['value']['price_unit'], res['value']['block_price']))
                 if price > res['value']['price_unit']:
@@ -102,18 +110,15 @@ class sale_order_line(osv.osv):
                         'message':
                         _("You have a price unit lower than the minimal\nYou cannot confirm your sale order, please ask to your manager to do it.")
                     }
-
                 res['warning'] = warning
-
         return res
 
-    def onchange_price_unit(self, cr, uid, ids, price_unit, block_price):
+    def onchange_price_unit(self, cr, uid, ids, price_unit, block_price, context=None):
         """
         If price unit is lower than block price, send a warning
         """
         if price_unit and block_price and (price_unit < block_price):
             _logger.debug('Price unit: %f, Block price: %f' % (price_unit, block_price))
-            context = self.pool.get('res.users').context_get(cr, uid)
             return {
                 'warning': {
                     'title': _('The unit price is lower than the price unit'),
